474b5f2acd8d78904b23388a524e21f08709ed34
WGrav01 <63617338+WGrav01@users.noreply.github.com>
2025-07-15 18:08:53 -0400

    Main activity (stolen from template)

--- app/src/main/java/org/wgrav/niceconvert/MainActivity.kt
+++ app/src/main/java/org/wgrav/niceconvert/MainActivity.kt
@@ -1,6 +1,1 @@
 package org.wgrav.niceconvert
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
